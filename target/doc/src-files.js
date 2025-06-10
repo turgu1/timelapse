a2bb66a5894ceba8ef86c59cf94a06e597bd046a
Guy Turcotte <turgu666@gmail.com>
2025-06-10 09:06:37 -0400

    Initial commit

---
+++ target/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["profiler",["",[],["lib.rs","profiler.rs"]]],["timelapse",["",[],["lib.rs","profiler.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[45,47]}
---
+++ target/doc/timelapse/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"macro":["profile_end","profile_start"],"mod":["profiler"]};
